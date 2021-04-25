7364477208f175fcf9877bb7d1d1b3a81caa0512
tieuconghoa <tieuconghoa193@gmail.com>
2021-04-25 12:29:45 +0700

    'better'

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ const cum123 = "1,4,9,15,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,3
 const cum45 = "51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,73,74,75";
 const cum67 = "76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95";
 const cum89 = "96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115";
-const cum10 = "";
-const cum11 = "116,117,118,119,120,121,122,123,124,125,126,127,128,129,130";
-const cum12 = "";
-const cum13 = "131,132,133,134,135,136,137,138,139,140";
-const cum14 = "141,142,143,144,145";
-const array = ["0", cum123, cum45, cum67, cum89, cum10, cum11, cum12, cum13, cum14];
+const cum1011 = "116,117,118,119,120,121,122,123,124,125,126,127,128,129,130";
+const cum1213 = "131,132,133,134,135,136,137,138,139,140";
+const array = [cum123, cum45, cum67, cum89, cum1011, cum1213];
 
 var app = express();
 var server = app.listen(process.env.PORT || 3000, () => {
@@ -42,21 +39,27 @@ app.use(function(req, res, next) {
 app.get("/api/:id", async(req, res) => {
     var id = req.params.id;
 
-    res.json(await get_data(encodeURIComponent(array[id - 1])));
+    res.json(await get_data(encodeURIComponent(array[id])));
 
+});
+
+app.get("/oldData", (req, res) => {
+    let rawdata = fs.readFileSync('./data/bxh_1619186400568.json');
+    let data = JSON.parse(rawdata);
+    res.json(data)
 });
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-function writeFile() {
+async function writeFile() {
 
     var data = [];
-    array.forEach(async(element) => {
-        var dataPush = await get_data(encodeURIComponent(element));
+    // array.forEach(async(element) => {
+        var dataPush = await get_data("21%2C22%2C23%2C24%2C25");
         data.push(dataPush);
-    });
+    // });
     var filename = "data/bxh_" + Date.now() + ".json";
     setTimeout(() => {
         data = "[" + data + "]";
@@ -67,6 +70,7 @@ function writeFile() {
         });
     }, 3000);
 };
+
 
 async function get_data(id) {
     var data = [];
