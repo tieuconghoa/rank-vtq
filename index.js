c71c34e72f5e77e5c54a72ee2e07d3f5642b631e
tieuconghoa <tieuconghoa193@gmail.com>
2021-04-28 19:18:01 +0700

    better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require("fs");
 const express = require("express");
 const path = require('path');
-const cron = require('node-cron');
+// const cron = require('node-cron');
 
 const cum123 = "1,4,9,15,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50";
 const cum45 = "51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,73,74,75";
@@ -37,9 +37,9 @@ app.use(function(req, res, next) {
     next();
 });
 
-cron.schedule('0 21 * * *', function() {
-    writeFile()
-  });
+// cron.schedule('0 21 * * *', function() {
+//     writeFile()
+//   });
 
 app.get("/api/:id", async(req, res) => {
     var id = req.params.id;
