fbfb773d1a60488d31a1e582e97f179d23506eef
tieuconghoa <tieuconghoa193@gmail.com>
2021-07-28 22:57:25 +0700

    better

--- index.js
+++ index.js
@@ -1,23 +1,131 @@
 const axios = require('axios');
 const fs = require("fs");
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const path = require('path');
 
-const cum123 = "1,4,9,15,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50";
-const cum45 = "51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,73,74,75";
-const cum67 = "76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95";
-const cum89 = "96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115";
-const cum1011 = "116,117,118,119,120,121,122,123,124,125,126,127,128,129,130";
-const cum1213 = "131,132,133,134,135,136,137,138,139,140";
-const array = [cum123, cum45, cum67, cum89, cum1011, cum1213];
+const array = [
+    {
+        "group": "1",
+        "server_start": "1",
+        "server_end": "20"
+    }, {
+        "group": "2",
+        "server_start": "21",
+        "server_end": "35"
+    }, {
+        "group": "3",
+        "server_start": "36",
+        "server_end": "50"
+    }, {
+        "group": "4",
+        "server_start": "51",
+        "server_end": "65"
+    }, {
+        "group": "5",
+        "server_start": "66",
+        "server_end": "75"
+    }, {
+        "group": "6",
+        "server_start": "76",
+        "server_end": "85"
+    }, {
+        "group": "7",
+        "server_start": "86",
+        "server_end": "95"
+    }, {
+        "group": "8",
+        "server_start": "96",
+        "server_end": "105"
+    }, {
+        "group": "9",
+        "server_start": "106",
+        "server_end": "115"
+    }, {
+        "group": "10",
+        "server_start": "116",
+        "server_end": "125"
+    }, {
+        "group": "11",
+        "server_start": "126",
+        "server_end": "130"
+    }, {
+        "group": "12",
+        "server_start": "131",
+        "server_end": "135"
+    }, {
+        "group": "13",
+        "server_start": "136",
+        "server_end": "140"
+    }, {
+        "group": "14",
+        "server_start": "141",
+        "server_end": "145"
+    }, {
+        "group": "15",
+        "server_start": "146",
+        "server_end": "150"
+    }, {
+        "group": "16",
+        "server_start": "151",
+        "server_end": "155"
+    }, {
+        "group": "17",
+        "server_start": "156",
+        "server_end": "160"
+    }, {
+        "group": "18",
+        "server_start": "161",
+        "server_end": "165"
+    }, {
+        "group": "19",
+        "server_start": "166",
+        "server_end": "170"
+    }, {
+        "group": "20",
+        "server_start": "171",
+        "server_end": "175"
+    }, {
+        "group": "21",
+        "server_start": "176",
+        "server_end": "180"
+    }, {
+        "group": "22",
+        "server_start": "181",
+        "server_end": "185"
+    }, {
+        "group": "23",
+        "server_start": "186",
+        "server_end": "190"
+    }, {
+        "group": "24",
+        "server_start": "191",
+        "server_end": "195"
+    }, {
+        "group": "25",
+        "server_start": "196",
+        "server_end": "200"
+    }, {
+        "group": "26",
+        "server_start": "201",
+        "server_end": "205"
+    }
+]
 
 var app = express();
 var server = app.listen(process.env.PORT || 3000, () => {
     console.log("server running!!!");
-})
+});
+app.use(bodyParser.urlencoded({
+    extended : true
+}))
+
+app.use(bodyParser.json());
+
 
 // Add headers
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
@@ -35,37 +143,45 @@ app.use(function(req, res, next) {
     // Pass to next layer of middleware
     next();
 });
-
-app.get("/api/:id", async(req, res) => {
+app.post("/api/data", async(req, res) => {
+    let fromGroup = parseInt(req.body.fromGroup,10);
+    let toGroup = parseInt(req.body.toGroup, 10);
+    var dataPush = await get_data(fromGroup, toGroup);
+    res.json(dataPush);
+})
+app.get("/api/:id", async (req, res) => {
     var id = req.params.id;
 
-    res.json(await get_data(encodeURIComponent(array[id])));
+    res.json(await get_data(id));
 
 });
 
 app.get("/oldData", (req, res) => {
-    let rawdata = fs.readFileSync(__dirname+'/data/bxh_1619186400568.json');
+    let rawdata = fs.readFileSync(__dirname + '/data/bxh_1619186400568.json');
     let data = JSON.parse(rawdata);
     res.json(data)
 });
 
 app.get("/newData", (req, res) => {
-    let rawdata = fs.readFileSync(__dirname+'/data/bxh_1619618400004.json');
+    let rawdata = fs.readFileSync(__dirname + '/data/bxh_1619618400004.json');
     let data = JSON.parse(rawdata);
-    res.json(data)
+    res.json(data);
 });
 
+app.get("/listGroup", (req, res) => {
+    res.json(array);
+})
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 async function writeFile() {
 
     var data = [];
-    array.forEach(async(element) => {
+    array.forEach(async (element) => {
         var dataPush = await get_data(encodeURIComponent(element));
         data.push(dataPush);
-     });
+    });
     var filename = "data/bxh_" + Date.now() + ".json";
     setTimeout(() => {
         data = "[" + data + "]";
@@ -77,30 +193,39 @@ async function writeFile() {
     }, 3000);
 };
 
+async function get_data(fromGroup, toGroup) {
+        var server_list = "";
+        for(var i = fromGroup; i <= toGroup; i++){
+            let server = array.find(ele => ele.group == i);
+            
+            for (j = server.server_start; j <= server.server_end; j++) {
+                server_list += "," + j;
+            }
+        }
+        
+        server_list = encodeURIComponent(server_list.substring(1));
+        var data = [];
+        await axios.get("https://vothan3q.playfun.vn/ApiF64vn/getRanks1?WorldID=" + server_list, {
+            "headers": {
+                "accept": "application/json, text/javascript, */*; q=0.01",
+                "accept-language": "en-US,en;q=0.9,vi;q=0.8",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-requested-with": "XMLHttpRequest"
+            },
+            "referrer": "https://vothan3q.playfun.vn/intro",
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": null,
+            "method": "GET",
+            "mode": "cors",
+            "credentials": "include"
+        }).then(response => {
 
-async function get_data(id) {
-    var data = [];
-    await axios.get("https://vothan3q.playfun.vn/ApiF64vn/getRanks1?WorldID=" + id, {
-        "headers": {
-            "accept": "application/json, text/javascript, */*; q=0.01",
-            "accept-language": "en-US,en;q=0.9,vi;q=0.8",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "x-requested-with": "XMLHttpRequest"
-        },
-        "referrer": "https://vothan3q.playfun.vn/intro",
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": null,
-        "method": "GET",
-        "mode": "cors",
-        "credentials": "include"
-    }).then(response => {
+            data = JSON.stringify(response.data.data.ranks);
 
-        data = JSON.stringify(response.data.data.ranks);
-
-    }).catch(error => {
-        console.log(error);
-    });
-    return data;
+        }).catch(error => {
+            console.log(error);
+        });
+        return data;
 };
